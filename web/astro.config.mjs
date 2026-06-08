ded8836553ac2c224506e19f90cbe8217661ce0e
Ville Launiala <ville.launiala@huesky.fi>
2026-06-09 00:14:37 +0300

    fix: site-URL netlify-previewille

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://amisfits.fi',
+  // site: 'https://amisfits.fi', // Vaihda takaisin kun domain on rekisteröity
+  site: 'https://amisfits-fi.netlify.app',
   // Kaikki sivut buildataan staattisiksi HTML-tiedostoiksi
   output: 'static',
   // Sitemap lisätään myöhemmin kun domain on rekisteröity
