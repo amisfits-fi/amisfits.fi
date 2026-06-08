bbe8ae1924552169a57ac987e8704d5b5bc392d6
Ville Launiala <ville.launiala@huesky.fi>
2026-06-08 22:52:54 +0300

    chore: lisää Sanity CLI config ja studioHost

---
+++ studio/sanity.cli.ts
@@ -0,0 +1,9 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'l442zg5l',
+    dataset:   'production',
+  },
+  studioHost: 'amisfits',
+})
